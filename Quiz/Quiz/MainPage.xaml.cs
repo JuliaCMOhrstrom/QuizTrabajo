2381056736ba296aef6484329f23fd40fc84694f
JuliaCMOhrstrom <19002494@alumnos.utleon.edu.mx>
2023-05-29 01:11:02 -0600

    Agregar archivos de proyecto.

---
+++ Quiz/Quiz/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Quiz
+{
+    public partial class MainPage : ContentPage
+    { 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void es(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Espanol());
+        }
+        private async void btnMa_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Matematicas());
+        }
+        private async void btnHi_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Historia());
+        }
+        private async void btnGe_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Geografia());
+        }
+
+    }
+}
---
+++ Quiz/Quiz/PreguntasBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    public abstract class PreguntasBuilder
+    {
+        protected Preguntas QuestionQuiz;
+        public Preguntas gettingQuestion() { return QuestionQuiz; }
+        public void CreandoInstancia() => QuestionQuiz = new Preguntas();
+
+
+
+
+        public abstract void Building(Preguntas pregunta);
+    }
+}
